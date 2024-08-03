30e70490f2f9024de2b3f6cab472b86d262646e5
Saim Ahmed <saim.ahmed.2781@gmail.com>
2024-08-03 16:39:50 +0500

    add

---
+++ 1.js
@@ -0,0 +1,58 @@
+// console.log(document.getElementById("black"))
+
+// console.dir(obj.outerText="SONGS")
+// let img=document.getElementsByClassName("card-img");
+// console.log("-----------------");
+// console.dir(img);
+// console.log("-----------------");
+// for (let i=0;i<img.length;i++){
+//     console.log(`The source of img ${i} `);
+//     console.dir(img[i].src);
+//     }
+
+//     console.log("-----------------");
+// console.log(document.getElementsByTagName("p"))
+// let p=document.getElementsByTagName("p");
+// for (let i=0;i<p.length-5;i++){
+//         console.log(`The  ${i} paragraph is `);
+//          console.dir(p[i].outerText="");
+//          }
+
+// console.dir(document.querySelectorAll("p"))
+// let ob=document.querySelectorAll("p");
+
+// console.dir(document.querySelectorAll(".card-img"))
+
+
+// console.dir(document.querySelector("#bg1"));
+// console.log(document.querySelector("#bg1").outerHTML="Saim Ahmed is")
+// console.dir(document.querySelector(".card"));
+// console.log(document.querySelectorAll(".card").outerHTML="Saim Ahmed is an engineer");
+
+// console.dir(document.querySelector("p").innerText="My name is <b>Saim Ahmed<b> ");
+// console.dir(document.querySelector("p").textContent="My name is <b>Saim Ahmed<b> ");
+// console.dir(document.querySelector("p").innerHTML="My name is <b>Saim Ahmed<b> ");
+
+// let image=document.querySelector('img')
+// console.dir(image)
+// console.log(image.style)
+
+// let para=document.querySelector('p')
+// console.dir(para)
+// console.log(para.style)
+
+// console.log(para.classList.add("boxp1"))
+// console.log("The presence of class is", para.classList.contains("boxp1"))
+
+// console.log(para.classList.toggle("boxp1"))
+// console.log("The presence of class is",para.classList.contains("boxp1"))
+// console.log(para.classList.toggle("boxp1"))
+
+// let nav=document.querySelector("a")
+// console.dir(nav.children)
+// console.log(nav.children[1])
+// console.log(nav.parentElement)
+
+function sayhi(){
+    alert("Hi")
+}
